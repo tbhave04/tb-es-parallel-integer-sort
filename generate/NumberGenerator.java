a18d3bf0c2308affd81071cfa465b438391d61af
Bryant Park <bryantpark04@gmail.com>
2025-02-11 18:56:13 -0500

    Fix number generator argument parsing

--- generate/NumberGenerator.java
+++ generate/NumberGenerator.java
@@ -5,8 +5,8 @@ import java.util.Random;
 class NumberGenerator {
     static int N = 100_000;
     public static void main (String[] args) {
-        if (args.length > 2) {
-            N = Integer.parseInt(args[2]);
+        if (args.length > 0) {
+            N = Integer.parseInt(args[0]);
         }
         Random gen = new Random();
         for (int i = 0; i < N; i++) {
